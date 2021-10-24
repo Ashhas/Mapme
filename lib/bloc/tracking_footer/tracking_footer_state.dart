3a256f3abcd25082836806226d4a3525836331a3
Aschwin B <aschwinbruyning@outlook.com>
2021-10-24 14:05:14 +0200

    + Added old UI

---
+++ lib/bloc/tracking_footer/tracking_footer_state.dart
@@ -0,0 +1,26 @@
+part of 'tracking_footer_bloc.dart';
+
+@immutable
+abstract class TrackingFooterState extends Equatable {
+  const TrackingFooterState();
+}
+
+class TrackingFooterInitState extends TrackingFooterState {
+  @override
+  List<Object> get props => [];
+}
+
+class HomeOpenedState extends TrackingFooterState {
+  @override
+  List<Object> get props => [];
+}
+
+class TrackingFooterRowOpenedState extends TrackingFooterState {
+  @override
+  List<Object> get props => [];
+}
+
+class TrackingFooterCardOpenedState extends TrackingFooterState {
+  @override
+  List<Object> get props => [];
+}
