cfa2a6df6ee7ef50aa88a833881a9a7de3f2669f
Aschwin B <aschwinbruyning@outlook.com>
2021-10-26 01:43:49 +0200

    Moved initial camera coordinates to constant

--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -1,3 +1,6 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 class Constants {
   static const String APIKEY = "<-- YOUR API KEY -->";
+  static const LatLng INITIAL_CAMERA_COORDINATES = LatLng(20.5937, 78.9629);
 }
