c68ce5d8f8339ad61787ccd88d85e08f6728fc98
Aschwin B <aschwinbruyning@outlook.com>
2021-10-26 01:45:25 +0200

    dart analysis refactor

--- lib/bloc/tracking_footer/tracking_footer_bloc.dart
+++ lib/bloc/tracking_footer/tracking_footer_bloc.dart
@@ -1,7 +1,5 @@
 import 'dart:async';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:meta/meta.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
